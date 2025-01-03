f44c281215a76b9803dedbdda8a3533ed4a1a083
HoneyBadgerrx <Katoyebir@gmail.com>
2025-01-03 12:20:46 +0100

    first push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+          <p style={{textAlign: 'center', color: 'white', paddingTop: '1rem'}}>Face Recogition Webapp</p>
+          <canvas></canvas>
+      </div>
+    </>
+  )
+}
+
+export default App
